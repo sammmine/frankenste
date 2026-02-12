6c424f28c0eb3e2f57d55e04b4230ef12d0810dd
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2026-02-12 15:45:01 +0700

    style: layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,18 +1,10 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+// components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.Flex({
-      components: [
-        { Component: Component.PageTitle() },
-        { Component: Component.Spacer(), grow: true },
-        { Component: Component.Search() },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -22,22 +14,55 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+// components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
   right: [
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
 
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
-  left: [],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+    Component.Explorer(),
+  ],
   right: [],
 }
